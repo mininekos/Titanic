d85f4859050f40da8c7fc2bb206529c4a1e15c8a
mininekos <manuel2g2m@gmail.com>
2022-09-19 13:45:34 +0200

    v2

--- Titanic/src/DAOs/DAOPersonas.java
+++ Titanic/src/DAOs/DAOPersonas.java
@@ -2,14 +2,30 @@ package DAOs;
 
 import java.util.ArrayList;
 
+import Enums.TipoZona;
+import POJO.Pasajero;
 import POJO.Persona;
+import POJO.Tripulación;
 
 public class DAOPersonas {
 	
-	private ArrayList<Persona> DaoPersonas;
+	private static ArrayList<Persona> DaoPersonas;
 	
 	public ArrayList<Persona> getDaoPersonas() {
-		return DaoPersonas;
+		if (DaoPersonas==null)
+			return DaoPersonas;
+		else
+			return DaoPersonas;
+	}
+	
+	private DAOPersonas() {
+		DaoPersonas=new ArrayList<Persona>();
+		
+		DaoPersonas.add(new Pasajero("145236978B", "Juan", TipoZona.BABOR, null, null, null, null));
+		DaoPersonas.add(new Pasajero(null, null, null, null, null, null, null));
+		
+		DaoPersonas.add(new Tripulación(null, null, null, null, null, null, null));
+		DaoPersonas.add(new Tripulación(null, null, null, null, null, null, null));
 	}
 	
 }

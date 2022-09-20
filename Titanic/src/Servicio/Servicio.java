533bddc6568e8a47185314bbc9ccbaf784009a2b
Manuel Jesús Guerrero Merino <manuel2g2m@gmail.com>
2022-09-20 19:44:58 +0200

    V3.2
    Ordenados

--- Titanic/src/Servicio/Servicio.java
+++ Titanic/src/Servicio/Servicio.java
@@ -6,24 +6,48 @@ import java.util.Map;
 
 import DAOs.DAOBotes;
 import DAOs.DAOPersonas;
+import Enums.TipoZona;
 import POJO.Persona;
 import POJO.Bote;
 
 public class Servicio {
 
-	private Map<Persona,Persona> personas ;
-
+	
+	private Map<Bote,ArrayList<Persona>> bote_persona;
 	private ArrayList<Persona> listaPersonas;
 	private ArrayList<Bote> listaBotes;
 	
 	public Servicio() {
 		super();
-		//this.personas = new HashMap<Persona,Persona>();
-		;
+		
 		this.listaPersonas = DAOPersonas.getInstance().getDaoPersonas();
 		this.listaBotes = DAOBotes.getInstance().getDaoBotes();
 	}
 	
+	
+	 public void llenarBote() {
+		 for (int ind = 0; ind < listaPersonas.size(); ind++) {
+			if(listaPersonas.get(ind).getZona()==TipoZona.PROA) {
+				
+			}
+			else if((listaPersonas.get(ind).getZona()==TipoZona.ESTRIBOR)) {
+				
+			}
+			else if((listaPersonas.get(ind).getZona()==TipoZona.POPA)) {
+				
+			}
+			else if((listaPersonas.get(ind).getZona()==TipoZona.BABOR)) {
+				
+			}
+		}
+		 		
+	 }
+	
+	private int comprobobarEdad() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
 	public String personasToString() {
 		
 		return listaPersonas.toString();
